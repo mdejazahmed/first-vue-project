3603f883fe86ab58ca2684ce6b890c7864f44b07
ejaz <rehanrumi0786@gmail.com>
2023-10-06 17:50:18 +0530

    first commit

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
 const app = createApp(App)
+
+// app.use(vue3GoogleLogin, {
+//   clientId: '762622375439-9i0f64683rbljqon9kgoui7jmmtce440.apps.googleusercontent.com'
+// })
+
 
 app.use(createPinia())
 app.use(router)
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,48 +1,49 @@
-<script setup>
-import { reactive, ref } from 'vue';
-import { RouterLink } from 'vue-router';
-import router from '../router';
-import { useAuthStore } from '../stores/counter';
+<script>
+export default {
+  methods: {
+    signInWithGoogle() {
+      const clientId = '762622375439-9i0f64683rbljqon9kgoui7jmmtce440.apps.googleusercontent.com';
+      const redirectUri = 'http://localhost:5173'; // Should match the one in your Google Cloud Console
+      const responseType = 'token id_token';
+      const scope = 'profile email';
 
+      const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
 
-const userDetails = reactive({
-    username: "",
-    password: ""
-});
+      // Open Google Sign-In in a new window
+      const popup = window.open(authUrl, '_blank', 'width=600,height=600');
 
-let msg = ref("")
+      // Listen for the window to close and handle the result
+      const checkPopupClosed = setInterval(() => {
+        if (popup.closed) {
+          clearInterval(checkPopupClosed);
+          this.handleSignInResult();
+        }
+      }, 1000);
+    },
 
-const login = async (userDetails) => {
-if(userDetails.username && userDetails.password){
-    const isValidate = await useAuthStore().login(userDetails)
-    console.log(isValidate)
-    if (isValidate) {
-        router.replace("/")
-    } else {
-        msg.value = "Invalid username or password"
-    }
-}else{
-    msg.value="Please enter username or password"
-}
-    
+    handleSignInResult() {
+      // Parse the URL of the current window to get the token
+      const params = new URLSearchParams(window.location.hash.substr(1));
+      const idToken = params.get('id_token');
+
+      if (idToken) {
+        // Successfully signed in, you can now use the idToken for authentication
+        console.log('Google ID Token:', idToken);
+      } else {
+        // Handle sign-in error
+        console.error('Google Sign-In Error');
+      }
+    },
+  },
 };
 </script>
 
 
 <template>
-    <div class="login">
-        <div class="form">
-            <input type="text" v-model="userDetails.username" placeholder="Username" required>
-            <input type="password" v-model="userDetails.password" placeholder="Password" required>
-            <button @click="login(userDetails)">Login</button>
-            <p v-if="msg" style="color: rgb(249, 52, 52);">{{ msg }}</p>
-
-            <p>
-                <RouterLink to="/signup">Signup</RouterLink>
-            </p>
-        </div>
+    <div>
+      <button @click="signInWithGoogle">Sign In with Google</button>
     </div>
-</template>
+  </template>
 
 <style scoped>
 .login {
@@ -51,13 +52,9 @@ if(userDetails.username && userDetails.password){
     display: flex;
     align-items: center;
     justify-content: center;
+    background: #F4F4F0;
 
 }
 
-.form {
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-}
+
 </style>
